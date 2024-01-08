ed8490a95d03a2f55da5ed668aeabeca47b04502
Abdullah Akhlaq <ch.abd.akh@gmail.com>
2024-01-08 16:09:58 +0500

    commented

--- list_comp.py
+++ list_comp.py
@@ -39,6 +39,5 @@ odd_nums = [i for i in range(1,11) if i%2 != 0]
 even_nums = [ i for i in range(1,11) if i%2 == 0]
 print(even_nums)
 print(odd_nums)
-
 square = [i**2 for i in range(1,11)]
 print(square)
